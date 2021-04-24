659b7a0e9aadef5c95e5a389a2d54df4f8f1f42c
yenmoc <phongsoyenmoc.diep@gmail.com>
2021-04-24 23:30:53 +0700

    *Update

--- Assets/_Root/Scripts/Runtime/PlayerHandle.cs
+++ Assets/_Root/Scripts/Runtime/PlayerHandle.cs
@@ -5,31 +5,53 @@ using UnityEngine.EventSystems;
 
 public class PlayerHandle : UnitHandle
 {
-   [SerializeField] private Rigidbody2D rigid2D;
-   [SerializeField] private Collider2D coll2D;
-   [SerializeField] private LeanSelectableByFinger leanSelectableByFinger;
+    [SerializeField] private Rigidbody2D rigid2D;
+    [SerializeField] private Collider2D coll2D;
+    [SerializeField] private LeanSelectableByFinger leanSelectableByFinger;
 
-   public void OnSelected()
-   {
-      rigid2D.gravityScale = 0;
-      coll2D.enabled = false;
-   }
-   
-   public void OnDeSelected()
-   {
-      rigid2D.gravityScale = 1;
-      coll2D.enabled = true;
-   }
+    private Vector3 _defaultPosition;
 
-   private void OnMouseDown()
-   {
-      OnSelected();
-      leanSelectableByFinger.SelfSelected = true;
-   }
+    private void Start() { UpdateDefaultPosition(); }
 
-   private void OnMouseUp()
-   {
-      OnDeSelected();
-      leanSelectableByFinger.Deselect();
-   }
+    public void UpdateDefaultPosition() { _defaultPosition = transform.localPosition; }
+
+    public bool CheckCorrectArea() { return false; }
+
+    public void ResetPlayerState() { transform.localPosition = _defaultPosition; }
+
+    public void OnSelected()
+    {
+        rigid2D.gravityScale = 0;
+        coll2D.enabled = false;
+    }
+
+    public void OnDeSelected()
+    {
+        rigid2D.gravityScale = 1;
+        coll2D.enabled = true;
+    }
+
+    private void OnMouseDown()
+    {
+        OnSelected();
+        leanSelectableByFinger.SelfSelected = true;
+    }
+
+    private void OnMouseUp()
+    {
+        var checkArea = CheckCorrectArea();
+
+        if (checkArea)
+        {
+            OnDeSelected();
+            leanSelectableByFinger.Deselect();
+        }
+        else
+        {
+            ResetPlayerState();
+            OnDeSelected();
+            leanSelectableByFinger.Deselect();
+            // display effect
+        }
+    }
 }
