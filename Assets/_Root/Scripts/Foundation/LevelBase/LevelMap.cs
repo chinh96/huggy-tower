01369c83fb8b8ecb3a1b8ff8d8670d47316971fd
longnk <longnk@gameestudio.com>
2021-05-25 17:14:50 +0700

    Fix level

--- Assets/_Root/Scripts/Foundation/LevelBase/LevelMap.cs
+++ Assets/_Root/Scripts/Foundation/LevelBase/LevelMap.cs
@@ -10,11 +10,15 @@ public class LevelMap : MonoBehaviour
     public int CurrentRealLevelIndex { get; private set; }
     public int CurrentFakeLevelIndex { get; private set; }
     public List<IUnit> Units { get; private set; }
+    public Vector3 offset = new Vector3(0, -150, 0);
 
     private void Start()
     {
         homeTower = GetComponentInChildren<HomeTower>();
         visitTower = GetComponentInChildren<VisitTower>();
+
+        homeTower.transform.localPosition += offset;
+        visitTower.transform.localPosition += offset;
     }
 
     public void SetLevelLoaded(int realLevelIndex, int fakeLevelIndex)
