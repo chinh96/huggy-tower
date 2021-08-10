97511452a98787b007083ecd26e4e358f2a8b0ea
longnk <longnk@gameestudio.com>
2021-08-10 10:44:08 +0700

    Fix facebook

--- Assets/_Root/Scripts/Popups/FacebookPopup.cs
+++ Assets/_Root/Scripts/Popups/FacebookPopup.cs
@@ -32,6 +32,10 @@ public class FacebookPopup : Popup
 
     private void CheckBackground()
     {
+        if (Data.JoinFbProgress > 2)
+        {
+            Data.JoinFbProgress = 0;
+        }
         backgrounds[Data.JoinFbProgress].SetActive(true);
         coinTotal.SetActive(Data.JoinFbProgress < 2);
     }
@@ -42,7 +46,7 @@ public class FacebookPopup : Popup
         {
             isClaming = true;
 
-            Data.JoinFbProgress++;
+            Data.JoinFbProgress = 1;
 
             ResourcesController.DailyQuest.IncreaseByType(DailyQuestType.LoginFacebook);
 
@@ -70,7 +74,7 @@ public class FacebookPopup : Popup
                 Data.CoinTotal++;
             }, () =>
             {
-                Data.JoinFbProgress++;
+                Data.JoinFbProgress = 2;
                 Data.CoinTotal = coinTotal;
                 ResourcesController.Achievement.IncreaseByType(AchievementType.JoinGroupFacebookSuccessfully);
                 Close();
