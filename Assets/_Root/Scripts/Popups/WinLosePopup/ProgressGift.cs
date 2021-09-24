5aec6120ce042411970ab2e2ecc31dc96fb9f25b
longnk <longnk@gameestudio.com>
2021-09-24 11:38:59 +0700

    Add % for progress win

--- Assets/_Root/Scripts/Popups/WinLosePopup/ProgressGift.cs
+++ Assets/_Root/Scripts/Popups/WinLosePopup/ProgressGift.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using System;
+using TMPro;
 
 public class ProgressGift : MonoBehaviour
 {
     [SerializeField] private Image progress;
     [SerializeField] private float duration;
+    [SerializeField] private TextMeshProUGUI text;
 
     public void Reset()
     {
         progress.fillAmount = Data.PercentProgressGift / 100f;
+        text.text = $"{Data.PercentProgressGift}%";
     }
 
     public void Move(Action action)
     {
-        Data.PercentProgressGift += ResourcesController.Config.PercentProgressGiftBonused;
+        int oldValue = Data.PercentProgressGift;
+        int newValue = Data.PercentProgressGift + ResourcesController.Config.PercentProgressGiftBonused;
+        if (newValue > 100)
+        {
+            newValue = 100;
+        }
+
+        Data.PercentProgressGift = newValue;
+
+        DOTween.To(() => oldValue, x =>
+        {
+            oldValue = x;
+            text.text = $"{oldValue}%";
+        }, newValue, duration);
+
         progress.DOFillAmount(Data.PercentProgressGift / 100f, duration).OnComplete(() =>
         {
             action?.Invoke();
